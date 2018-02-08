09fd1f5a44ea5981736b89f93d31fc9d934f3cd6
Saini <RSAINI@BECKMAN.COM>
2018-02-08 00:14:51 -0800

    Add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -48,6 +48,23 @@ app.post('/users', (req, res)=>{
     })
 });
 
+//**********************************************/
+//**************POST - /users/login*************/
+//**********************************************/
+
+app.post('/users/login', (req, res)=>{
+    var body = _.pick(req.body, ['email', 'password']); 
+    
+    User.findByCredentials(body.email, body.password).then((user)=>{
+        //res.send(user);
+        return user.generateAuthToken().then((token)=>{
+            res.header('x-auth', token).send(user);
+        })
+    }).catch((e)=>{
+        res.status(400).send();
+    });
+});
+
 
 
 //**********************************************/
